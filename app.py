5f470f29d73ca347e345ff7e4641e9cbd737ba10
David <davidhaw@udel.edu>
2026-03-31 22:34:27 -0400

    Started Doctor Home First Tab

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ if "page" not in st.session_state:
 if "role" not in st.session_state:
     st.session_state["role"] = None
 
+if "userid" not in st.session_state:
+    st.session_state["userid"] = None
+
 users = [
     {
         "id" : str(uuid.uuid4()),
@@ -43,10 +46,12 @@ appointments = [
         "appointment_id" : str(uuid.uuid4()),
         "patient_id" : None,
         "doctor_id" : "1",#Change ID
-        "date" : "03_20_2026",
+        "date" : "03/20/2026, 13:00",
+        "length": "30 minutes",
         "status" : "available"
     }
 ]
+possible_lengths = ["15 minutes", "30 minutes", "45 minutes", "60 minutes"]
 
 json_path_users = Path("users.json")
 json_path_appointments = Path("appointments.json")
@@ -60,15 +65,29 @@ if json_path_appointments.exists():
         appointments = json.load(f)
 
 with st.sidebar:
-    st.markdown("Patient Appointment Sidebar")
     if "logged_in" in st.session_state and st.session_state["logged_in"] != False:
         user = st.session_state["user"]
-        st.markdown(f"Welcome {user['email']}")
+        st.markdown(f"Welcome {user['name']}")
+        if st.button("Log Out", type = "primary", use_container_width=True):
+            with st.spinner("Logging out..."):
+                time.sleep(3)
+                st.session_state["page"] = "login"
+                st.session_state["logged_in"] = False
+                st.session_state["user"] = None
+                st.session_state["role"] = None
+                st.session_state["userid"] = None
+                st.rerun()
+
     else:
-        st.markdown("Welcome! - Login")
+        st.markdown("Welcome! - Login or Register")
+        if st.button("Login", type="primary", use_container_width=True):
+            st.session_state["page"]  = "login"
+        if st.button("Register", type = "primary", use_container_width=True, key="registerpage"):
+            st.session_state["page"]  = "register"
 
 
 if st.session_state["page"] == "login":
+    st.set_page_config("Patient Appointment Tracker", layout = "centered", initial_sidebar_state= "expanded")
     st.subheader("Log In")
     with st.container(border=True):
         email_input = st.text_input("Email Address", key = "email_address_login")
@@ -92,16 +111,18 @@ if st.session_state["page"] == "login":
                     st.session_state["user"] = found_user
                     st.session_state["role"] = found_user['role']
                     st.session_state['page'] = "home"
+                    st.session_state["userid"] = found_user['id']
                     time.sleep(2)
                     st.rerun()
                 else:
                     st.error("Invalid credentials")
-    url = st.session_state["page"] = "register"
+    url = "registerpage" # Ask about in class
     link_text = "Create Account"
     st.markdown(f"Don't have an account? [{link_text}]({url})")
         
 
-if st.session_state["page"] == "register":
+elif st.session_state["page"] == "register":
+    st.set_page_config("Patient Appointment Tracker", layout = "centered", initial_sidebar_state= "expanded")
     st.subheader("Register")
     with st.container(border=True):
         namereg_input = st.text_input("Full Name", key = "name_register")
@@ -131,11 +152,57 @@ if st.session_state["page"] == "register":
                     with json_path_users.open("w",encoding = "utf-8") as f:
                             json.dump(users, f, indent = 4)
                     st.success(f"Registered {namereg_input}!")
-                    st.session_state["logged_in"]= False
-                    st.session_state["user"] = None
-                    st.session_state["role"] = None
-                    st.session_state['page'] = "login"
                     time.sleep(2)
                     st.rerun()
-if st.session_state["page"] == "home":
-    st.markdown("home")
+
+if st.session_state["role"] == "Patient":
+    if st.session_state["page"] == "home":
+        st.set_page_config("Patient Appointment Tracker", layout = "wide", initial_sidebar_state= "expanded")
+        st.header("Patient Home") 
+        st.divider()
+        tab11, tab22 = st.tabs(["View Available Appointments", "Schedule an Appointment"], width="stretch")
+        with tab11:
+            pass
+        with tab22:
+            pass
+
+elif st.session_state["role"] == "Doctor":
+    if st.session_state["page"] == "home":
+        st.set_page_config("Patient Appointment Tracker", layout = "wide", initial_sidebar_state= "expanded")
+        st.header("Doctor Home")
+        st.divider()
+        tab1, tab3, tab4 = st.tabs(["Create Availabilty", "Update Appointments", "Delete Appointments"], width= "stretch")
+        with tab1:
+            appt_dateandtime = st.datetime_input("Date and Time Available")
+            appt_length = st.selectbox("Appointment Length", possible_lengths)
+            if st.button("Create Appointment", key = "apptcreatebtn", type="primary", use_container_width=True):
+                with st.spinner("Creating..."):
+                    time.sleep(2)
+                    appt_exists = False
+                    for appointment in appointments:
+                        if (appointment["date"] == str(appt_dateandtime)) & (appointment["doctor_id"] == st.session_state["userid"]):
+                            appt_exists = True
+                            st.error("Appointment Already Exists!")
+                            break
+                    if not appt_exists:
+                        appointments.append(
+                            {
+                                "appointment_id" : str(uuid.uuid4()),
+                                "patient_id" : None,
+                                "doctor_id" : st.session_state["userid"],
+                                "date" : str(appt_dateandtime),
+                                "length": appt_length,
+                                "status" : "available"
+                            }
+                        )
+                        with json_path_appointments.open("w",encoding = "utf-8") as f:
+                            json.dump(appointments, f, indent = 4)
+                        st.success(f"Appointment Created {appt_dateandtime}")
+
+        with tab3:
+            pass
+
+        with tab4:
+            pass
+
+
